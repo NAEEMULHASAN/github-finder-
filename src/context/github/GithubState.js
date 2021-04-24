bbda11d7fc32eddc129f3c52f0149693d9f41e8e
NAEEMULHASAN <w2487290@gmail.com>
2021-04-24 17:09:42 +0000

    to publish the repo
    all set, netlify init  done

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -4,6 +4,20 @@ import GithubContext from './githubContext';
 import GithubReducer from './githubReducer';
 import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER, GET_REPOS } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production') {
+        githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+        githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+
+}
+else{
+    githubClientId = process.env.GITHUB_CLIENT_ID;
+        githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+
+}
+
 const GithubState = props => {
     const initialState = {
         users: [],
@@ -16,7 +30,7 @@ const GithubState = props => {
     //This function is called from the search component by passing props up
     const searchUsers = async (text) => {
         setLoading();
-        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
         dispatch({
             type: SEARCH_USERS,
             payload: res.data.items 
@@ -27,7 +41,7 @@ const GithubState = props => {
     //This function is called from the UserItem component to display User Details
   const getUser = async (username) => {
     setLoading();
-    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`);
      
     dispatch({
         type: GET_USER,
@@ -40,7 +54,7 @@ const GithubState = props => {
     //This fumction is called from the user.js component to display the latest repos from Github
     const getUserRepos = async (username) => {
         setLoading();
-        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
        dispatch({
            type: GET_REPOS,
            payload: res.data
